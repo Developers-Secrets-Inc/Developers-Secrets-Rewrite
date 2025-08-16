03c351c761bd99931ed4813c17934b5e14879dbf
AliiiBenn <davidddpereiraaa6@gmail.com>
2025-08-16 14:59:26 +0200

    Update

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,16 @@ const nextConfig = {
 
     return webpackConfig
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
