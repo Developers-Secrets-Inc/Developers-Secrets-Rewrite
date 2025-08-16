52711f22162050d468fd72ac7946b5916deaf33f
AliiiBenn <davidddpereiraaa6@gmail.com>
2025-08-16 15:26:59 +0200

    Update

--- src/core/auth/components/buttons/github-auth-button.tsx
+++ src/core/auth/components/buttons/github-auth-button.tsx
@@ -3,10 +3,26 @@
 import { GithubIcon } from '@/components/icons/github'
 import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
+import { signInWithGithub } from '@/core/auth'
+import { redirect } from 'next/navigation'
+import { isFailure } from '@/core/fn/result'
 
 export function GitHubAuthButton() {
+  const handleClick = async () => {
+    const result = await signInWithGithub()
+    if (isFailure(result)) {
+      console.error(result.error)
+      return
+    }
+    redirect(result.value.url)
+  }
+
   return (
-    <Button className={cn('flex items-center gap-2 w-full')} variant="outline" disabled={true}>
+    <Button
+      className={cn('flex items-center gap-2 w-full cursor-pointer')}
+      variant="outline"
+      onClick={handleClick}
+    >
       <GithubIcon />
       Continue with GitHub
     </Button>
--- src/core/auth/components/buttons/google-auth-button.tsx
+++ src/core/auth/components/buttons/google-auth-button.tsx
@@ -3,10 +3,26 @@
 import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
 import { GoogleIcon } from '@/components/icons/google'
+import { signInWithGoogle } from '@/core/auth'
+import { redirect } from 'next/navigation'
+import { isFailure } from '@/core/fn/result'
 
 export function GoogleAuthButton() {
+  const handleClick = async () => {
+    const result = await signInWithGoogle()
+    if (isFailure(result)) {
+      console.error(result.error)
+      return
+    }
+    redirect(result.value.url)
+  }
+
   return (
-    <Button className={cn('flex items-center gap-2 w-full')} variant="outline" disabled={true}>
+    <Button
+      className={cn('flex items-center gap-2 w-full cursor-pointer')}
+      variant="outline"
+      onClick={handleClick}
+    >
       <GoogleIcon />
       Continue with Google
     </Button>
